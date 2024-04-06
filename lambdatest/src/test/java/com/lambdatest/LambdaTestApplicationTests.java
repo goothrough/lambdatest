7b49dd6aa08d19e02aee014ca17c368f91e94c97
s_ohashi <goothrough315@gmail.com>
2024-04-07 00:30:31 +0900

    first-commit

---
+++ lambdatest/src/test/java/com/lambdatest/LambdaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lambdatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LambdaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
