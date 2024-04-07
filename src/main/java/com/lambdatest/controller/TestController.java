8585a767937ef745409fd0f2bff2611a6b7fcd09
s_ohashi <goothrough315@gmail.com>
2024-04-07 18:16:25 +0900

    update

--- src/main/java/com/lambdatest/controller/TestController.java
+++ src/main/java/com/lambdatest/controller/TestController.java
@@ -3,13 +3,22 @@ package com.lambdatest.controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lambdatest.config.Settings;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
 public class TestController {
+
+	private final Settings settings;
 
 	@PostMapping("/hello")
 	public void test() {
 
 		System.out.println("Hello Lambda!");
+
+		System.out.println(settings.getAccountId());
 
 	}
 
